023ca89f31813f6165fdb81fc08c4682f442576d
Njau <njau.wordpress@gmail.com>
2025-03-06 18:14:33 +0300

    Video Categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,25 @@
 import { HydrateClient, trpc } from "@/trpc/server";
-// 4.10.52
 
-export default async function Home() {
+import { HomeView } from "@/modules/home/ui/views/home-view";
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string
+  }>
+}
+
+const Page = async ({ searchParams}: PageProps) => {
+  const { categoryId} = await searchParams;
+
   void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
 }
+
+export default Page
